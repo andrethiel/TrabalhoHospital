fcf8d3d7c03b2d035f10318c430f1450ee793b18
Lucas Resende Machado <43017929+lucresmac@users.noreply.github.com>
2020-12-07 21:36:20 -0300

    API CEP

--- HospitalWeb/Models/UserView.cs
+++ HospitalWeb/Models/UserView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,7 +15,23 @@ namespace HospitalWeb.Models
         public string Email { get; set; }
         public string Senha { get; set; }
         public string Setor { get; set; }
-        
-        
+
+        [Display(Name = "CEP")]
+        public string Cep { get; set; }
+
+        [Display(Name = "Estado")]
+        public string Uf { get; set; }
+
+        [Display(Name = "Cidade")]
+        public string Cidade { get; set; }
+
+        [Display(Name = "Bairro")]
+        public string Bairro { get; set; }
+
+        [Display(Name = "Tipo")]
+        public string TipoLogradouro { get; set; }
+
+        [Display(Name = "Endereço")]
+        public string Logradouro { get; set; }
     }
 }
